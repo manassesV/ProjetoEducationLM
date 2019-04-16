5e44cf595e26d5710a48579fc55da2df4039b1f5
ManassesV <manavitorino@gmail.com>
2019-04-15 21:46:40 -0300

    Login Success

---
+++ app/src/main/java/com/manasses/manab/project/data/local/entity/Atividade.kt
@@ -0,0 +1,12 @@
+package com.manasses.manab.project.data.local.entity
+
+class  Atividade(
+
+    var id: Int = 0,
+    var descricao: String = "",
+    var categoria: String = "",
+    var validade: String = "",
+    var tipo: String = "",
+    var tempo: String = ""
+
+)
